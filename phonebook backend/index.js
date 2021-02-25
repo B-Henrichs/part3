d69a637f8ea6dbbce28bafe6c3ad484a342646b1
Brian Henrichs <henbrc@gmail.com>
2021-02-25 11:39:39 -0800

    added delete and update functionality

--- phonebook backend/index.js
+++ phonebook backend/index.js
@@ -5,15 +5,24 @@ const app = express()
 // imports secret info from .env file
 require('dotenv').config()
 
-//import cors and use
+// imports mongoose and number(person) function
+const Number = require('./models/number')
+
+// import cors and use
 const cors = require('cors')
 app.use(cors())
 
 // informs express which format(I think)
 app.use(express.json()) 
 app.use(express.static('build'))
+
+/*
+
 // imports morgan
 const morgan = require('morgan')
+
+//  I think morgan has been replaced with 
+// another debugger
 // creates custom morgan token that has weird
 // decleration syntax required to function
 // *note* the body is generated off the request and
@@ -22,80 +31,110 @@ morgan.token("post", (req, res) => {
   const { body } = req;
   return JSON.stringify(body);
 });
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
-//imports mongoose and number(person) function
-const Number = require('./models/number')
+*/
 
-
-//   these handle reqests to the server
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
+  // handles requests to server 
+  // @ /persons
+  // added map method from answers page
+  // seemed to function before
   app.get('/api/persons', (request, response) => {
-    Number.find({}).then(people => {
-      response.json(people)
+    Number.find({}).then(persons => {
+      response.json(persons.map(person => person.toJSON()))
   })
 })
 
-  //info page
-  app.get('/info', (request, response) => {
-     const numberOfPeople= (persons.length)+1
+  // info page
+  app.get('/info', (request, response,) => {
+    
      const date= new Date()
-    response.send(`<p>Phonebook has info for ${numberOfPeople} people<p> <br/> <div>${date}(Pacific Standard Time)`)
+    response.send(`<p>Phonebook has info for  people<p> <br/> <div>${date}(Pacific Standard Time)`)
   })
 
 
-  //specific entrys
-  app.get('/api/persons/:id', (request, response) => {
-    Number.findById(request.params.id).then(person => {
-      response.json(person)
+  // specific entrys
+  app.get('/api/persons/:id', (request, response, next) => {
+    Number.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
 
-  // handles ID generation for the post function
-  /*const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-  }*/
+  
 
+// handles "post" requests to server
+  
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
 
-  // handles "post" requests to server
-  app.post('/api/persons', (request, response) => {
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }else if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  
+  const person = new Number({
+    name: body.name,
+    number: body.number || false
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  }).catch(error => next(error))
+})
+
+// handles delete requests
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Number.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-    
-
-    if (!body.number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
-    }else if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-    
-    const person = new Number({
+  
+    const person = {
       name: body.name,
-      number: body.number || false
-    })
+      number: body.number,
+    }
   
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
+    Number.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
   })
 
-
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
   
-    response.status(204).end()
-  })
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  app.use(errorHandler)
   
 
   const PORT = process.env.PORT
--- phonebook backend/mongo.js
+++ phonebook backend/mongo.js
@@ -1,30 +1,48 @@
+
+// imports "process" and "mongoose" libraries(?)
 const process = require('process')
 const mongoose = require('mongoose')
 
+// if the number of arguments passed when called 
+// in the command-line ask the user to pass it the password
 if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }
 
+// sets the password to the 2nd argument
+// passed in the command-line
 const password = process.argv[2]
 
+// sets the url to the addres providewd by atlas
 const url =
   `mongodb+srv://fullstack:${password}@cluster0.h3g38.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
+
+  // tells mongoose to connect to the url with the settings provided
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
+
+// defines how an entry will be stored in the Atlas database
 const numberSchema = new mongoose.Schema({
   name: String,
   number: String
 })
 
+// tells mongoose to use the schema above
 const Number = mongoose.model('Number', numberSchema)
 
+
+
+// handles creating a new entry with the 3rd and 4th arguments
+// passed from the command-line
 const number = new Number({
   name: process.argv[3],
   number: process.argv[4]
 })
 
+// if there isnt a third argument just print the phonebook
+// otherwise saves the number created above
 if(!process.argv[3]) {
   console.log('Phonebook:')
   Number.find({}).then(result => {
